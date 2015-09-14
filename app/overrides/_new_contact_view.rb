4af6eb2e220974f87acf2e73dbdda8981fe28073
Noel <noel@2bedigital.com>
2015-09-15 00:39:37 +0100

    bug on override

--- app/overrides/_new_contact_view.rb
+++ app/overrides/_new_contact_view.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => 'spree/contact_us/contacts/new',
                      :name => 'new_contact_view',
-                     :insert_before => "h1",
+                     :insert_after => "h1",
                      :partial => 'spree/contact_us/contacts/new')
