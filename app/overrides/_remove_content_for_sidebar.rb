dc161eadb4bd0694a2992fe2ce4b98783814fb71
Noel <noel@2bedigital.com>
2015-09-15 14:50:44 +0100

    provisional delete for overrides

--- app/overrides/_remove_content_for_sidebar.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/contact_us/contacts/new',
-                     :name => 'remove_content_for_sidebar',
-                     :sequence => {:before => 'remove_form_on_contact_us'},
-                     :remove => "erb[silent]:contains('content_for :sidebar do ')",
-                     :closing_selector => "erb[silent]:contains('end')")
--- app/overrides/_remove_form_on_contact_us.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/contact_us/contacts/new',
-                     :name => 'remove_form_on_contact_us',
-                     :sequence => {:before => 'new_contact_view'},
-                     :remove => "erb[silent]:contains('form_for @contact, :url => contacts_path, :html => { :novalidate => '' } do |f|')",
-                     :closing_selector => "erb[silent]:contains('end')")
