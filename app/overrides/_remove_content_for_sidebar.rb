cf1b3ebabf6c5af03460196272020dc167f5192a
Noel <noel@2bedigital.com>
2015-09-15 02:34:30 +0100

    bugs on overrides

--- app/overrides/_remove_content_for_sidebar.rb
+++ app/overrides/_remove_content_for_sidebar.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/contact_us/contacts/new',
                      :name => 'remove_content_for_sidebar',
                      :sequence => {:before => 'remove_form_on_contact_us'},
-                     :remove => "erb[loud]:contains('content_for :sidebar do ')")
+                     :remove => "erb[silent]:contains('content_for :sidebar do ')",
+                     :closing_selector => "erb[silent]:contains('end')")
