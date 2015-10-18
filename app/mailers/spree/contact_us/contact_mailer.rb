5fb48e0c0666df7dfca7b2d75b9acde98fb68a4b
SergiCarrasco <sergi@2bedigital.com>
2015-10-19 01:55:09 +0200

    Email from and to addresses configured in database.

---
+++ app/mailers/spree/contact_us/contact_mailer.rb
@@ -0,0 +1,10 @@
+class Spree::ContactUs::ContactMailer < Spree::BaseMailer
+  def contact_email(contact)
+    @contact = contact
+    mail :from     => (Spree::Config.mails_from || @contact.email),
+         :reply_to => @contact.email,
+         :subject  => (SpreeContactUs.require_subject ? @contact.subject : Spree.t(:subject, :email => @contact.email)),
+         :to       => Spree::Config.mail_bcc.split(",")[0],
+         :bcc      => ENV["ActionMailer_default_options_bcc"]
+  end
+end
